c9f5b473df8589573534ac7b1fbafbab030ce371
ANDRE-PC\ital- <ital-andre95@live.com>
2021-11-14 13:00:41 -0500

    file format

--- Nhl.Api.Extensions.Microsoft.DependencyInjection/Src/NhlApiServiceCollectionExtensions.cs
+++ Nhl.Api.Extensions.Microsoft.DependencyInjection/Src/NhlApiServiceCollectionExtensions.cs
@@ -2,36 +2,36 @@
 
 namespace Nhl.Api.Extensions.Microsoft.DependencyInjection
 {
-	/// <summary>
-	/// The extensions for the Nhl.Api for Microsoft Dependency Injection
-	/// </summary>
-	public static class NhlApiServiceCollectionExtensions
-	{
-		/// <summary>
-		/// Register's the Nhl.Api to the <see cref="IServiceCollection"/> for usage within the Microsoft dependency injection library
-		/// </summary>
-		/// <param name="services">The collection of services from the <see cref="IServiceCollection"/></param>
-		/// <param name="serviceLifetime">The type of service lifetime the Nhl.Api will be injected into the <see cref="IServiceCollection"/> </param>
-		/// <returns>Returns an updated <see cref="IServiceCollection"/> with the Nhl.Api included</returns>
-		public static IServiceCollection AddNhlApi(this IServiceCollection services, ServiceLifetime serviceLifetime = ServiceLifetime.Transient)
-		{
+    /// <summary>
+    /// The extensions for the Nhl.Api for Microsoft Dependency Injection
+    /// </summary>
+    public static class NhlApiServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Register's the Nhl.Api to the <see cref="IServiceCollection"/> for usage within the Microsoft dependency injection library
+        /// </summary>
+        /// <param name="services">The collection of services from the <see cref="IServiceCollection"/></param>
+        /// <param name="serviceLifetime">The type of service lifetime the Nhl.Api will be injected into the <see cref="IServiceCollection"/> </param>
+        /// <returns>Returns an updated <see cref="IServiceCollection"/> with the Nhl.Api included</returns>
+        public static IServiceCollection AddNhlApi(this IServiceCollection services, ServiceLifetime serviceLifetime = ServiceLifetime.Transient)
+        {
 
-			switch (serviceLifetime)
-			{
-				case ServiceLifetime.Singleton:
-					services.AddSingleton<INhlApi, NhlApi>();
-					break;
+            switch (serviceLifetime)
+            {
+                case ServiceLifetime.Singleton:
+                    services.AddSingleton<INhlApi, NhlApi>();
+                    break;
 
-				case ServiceLifetime.Scoped:
-					services.AddScoped<INhlApi, NhlApi>();
-					break;
+                case ServiceLifetime.Scoped:
+                    services.AddScoped<INhlApi, NhlApi>();
+                    break;
 
-				case ServiceLifetime.Transient:
-					services.AddTransient<INhlApi, NhlApi>();
-					break;
-			}
+                case ServiceLifetime.Transient:
+                    services.AddTransient<INhlApi, NhlApi>();
+                    break;
+            }
 
-			return services;
-		}
-	}
+            return services;
+        }
+    }
 }
